f844da8ceba54bdfcd8922d9375d05aa4c66ebb8
wczetyrbok <wczetyrbok@griddynamics.com>
2023-10-09 12:51:07 +0200

    delete implement Comparable

--- src/main/java/org/example/Citizen.java
+++ src/main/java/org/example/Citizen.java
@@ -5,21 +5,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.Comparator;
 
 @Getter
 @Setter
 @Builder
 
-public class Citizen implements Comparable<Citizen>, Serializable {
+public class Citizen implements Serializable {
     String name;
     int age;
     City city;
-
-    @Override
-    public int compareTo(Citizen otherCitizen) {
-        return Comparator.comparing(Citizen::getName)
-                .thenComparingInt(Citizen::getAge)
-                .compare(this, otherCitizen);
-    }
 }
--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -6,22 +6,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.Comparator;
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
-public class City implements Comparable<City>, Serializable {
+public class City implements Serializable {
     String name;
     double area;
     @EqualsAndHashCode.Exclude
     List<Citizen> citizens;
-
-    @Override
-    public int compareTo(City otherCompany) {
-        return Comparator.comparing(City::getName)
-                .thenComparingDouble(City::getArea)
-                .compare(this, otherCompany);
-    }
 }
