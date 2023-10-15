071d50c1239f2ba0cd066bd22a86b77e6d57af54
Wiktor <kasztanators2701@gmail.com>
2023-10-15 18:03:53 +0200

    add CitizenMapper

--- src/main/java/org/example/mapper/CityMapper.java
+++ src/main/java/org/example/mapper/CityMapper.java
@@ -4,21 +4,22 @@ import org.example.dto.CityDto;
 import org.example.model.City;
 
 public class CityMapper {
-    public static City mapToCity(CityDto cityDto){
-            return City.builder()
-                    .id(cityDto.getId())
-                    .area(cityDto.getArea())
-                    .name(cityDto.getName())
-                    .citizens(cityDto.getCitizens())
-                    .build();
+    public static City mapToCity(CityDto cityDto) {
+        return City.builder()
+                .id(cityDto.getId())
+                .area(cityDto.getArea())
+                .name(cityDto.getName())
+                .citizens(cityDto.getCitizens())
+                .build();
     }
-    public static CityDto mapToCityDto(City city){
-            return CityDto.builder()
-                    .id(city.getId())
-                    .area(city.getArea())
-                    .name(city.getName())
-                    .citizens(city.getCitizens())
-                    .build();
+
+    public static CityDto mapToCityDto(City city) {
+        return CityDto.builder()
+                .id(city.getId())
+                .area(city.getArea())
+                .name(city.getName())
+                .citizens(city.getCitizens())
+                .build();
     }
 
 
