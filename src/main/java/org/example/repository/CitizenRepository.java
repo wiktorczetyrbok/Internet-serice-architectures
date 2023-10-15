2d67e3af8017b9efe6eb9df38c8183d07ae8389b
Wiktor <kasztanators2701@gmail.com>
2023-10-15 18:03:33 +0200

    add repositories

---
+++ src/main/java/org/example/repository/CitizenRepository.java
@@ -0,0 +1,20 @@
+package org.example.repository;
+
+import org.example.model.Citizen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CitizenRepository extends JpaRepository<Citizen, UUID> {
+    @Override
+    List<Citizen> findAll();
+
+    @Query("select c from Citizen c where c.name = :name and c.age = :age")
+    Optional<Citizen> findCitizenByNameAndAge(
+            @Param("name") String name,
+            @Param("age") int age);
+}
