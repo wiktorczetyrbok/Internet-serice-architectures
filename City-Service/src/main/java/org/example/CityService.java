17b9ba5d774e9e7191b87b54395100991421b6ff
Wiktor <kasztanators2701@gmail.com>
2023-10-30 15:50:37 +0100

    refactor files

--- City-Service/src/main/java/org/example/CityService.java
+++ City-Service/src/main/java/org/example/CityService.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class CityService {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExampleApplication.class, args);
+        SpringApplication.run(CityService.class, args);
     }
 }
