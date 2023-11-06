a18308c8f459b21d64388f56091bc2d87acff5ae
Wiktor <kasztanators2701@gmail.com>
2023-11-06 18:28:33 +0100

    add initial gateway files

---
+++ City-Mangament-Gateway/src/main/java/org/isa/IsaApplication.java
@@ -0,0 +1,13 @@
+package org.isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaApplication.class, args);
+	}
+
+}
