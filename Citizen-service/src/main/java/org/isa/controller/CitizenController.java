8d1da6c071be059365d17f63712f1a3a3b4d86f7
Wiktor <kasztanators2701@gmail.com>
2023-11-07 14:48:49 +0100

    reformat endpoints to plural form

--- Citizen-service/src/main/java/org/isa/controller/CitizenController.java
+++ Citizen-service/src/main/java/org/isa/controller/CitizenController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/citizen")
+@RequestMapping("/citizens")
 public class CitizenController {
     private final CitizenService citizenService;
 
--- Citizen-service/src/main/java/org/isa/controller/CityController.java
+++ Citizen-service/src/main/java/org/isa/controller/CityController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/city")
+@RequestMapping("/cities")
 public class CityController {
     private final CityService cityService;
 
