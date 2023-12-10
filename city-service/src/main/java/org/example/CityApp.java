d46eab5b60931a9ce6bee4c85f2dc19afe8f6889
Wiktor <kasztanators2701@gmail.com>
2023-12-10 18:17:33 +0100

    configure eureka clients

--- city-service/src/main/java/org/example/CityApp.java
+++ city-service/src/main/java/org/example/CityApp.java
@@ -2,8 +2,11 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CityApp {
 
     public static void main(String[] args) {
